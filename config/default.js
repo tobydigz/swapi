414878181436cf92ea1a4243c5ff71ba4fae3d69
Oloruntobi Allen <oloruntobi.allen@gmail.com>
2019-05-26 16:06:45 +0100

    Add http request for swapi.co

--- config/default.js
+++ config/default.js
@@ -1,6 +1,7 @@
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
     logLevel: process.env.LOG_LEVEL || 'info',
+    SWAPI_URL: 'https://swapi.co/api',
     TABLE_NAME: process.env.TABLE_NAME || '',
     DB_PASSWORD: process.env.DB_PASSWORD || '',
     DB_NAME: process.env.DB_NAME || '',
---
+++ src/utils/SwapiRequest.js
@@ -0,0 +1,14 @@
+const request = require('request-promise-native');
+const config = require('config');
+const {SWAPI_URL: endpoint} = config;
+
+const fetchData = async (path, queryObject) => {
+    const options = {
+        uri: `${endpoint}/${path}`,
+        method: 'GET',
+        qs: queryObject,
+        simple: true,
+        json: true,
+    };
+    return request(options);
+};
